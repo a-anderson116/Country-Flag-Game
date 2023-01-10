4121b7af56de7d9fc66e6dd835dd356c2eb71421
Alex Anderson <andersonaj116@gmail.com>
2023-01-10 09:29:47 -0600

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Alex Anderson on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
